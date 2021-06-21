b082509e13fa95716db791c174f7e0c540832349
Minal <patilminalp@gmail.com>
2021-06-22 01:33:50 +0530

    Added:Using Switch case to solve empWage problem

--- Attendence.java
+++ Attendence.java
@@ -3,36 +3,25 @@ package com.program;
 public class Attendence extends EmpWage {
 
 
-    public void attendance(){
-        int IS_FULL_TIME=1;
-        int IS_PART_TIME=2;
-        int WAGE_PER_HRS=20;
-        int fullDayHr=8;
-        int partTimeHr=4;
-        int dailyEmpWage=0;
-
-        double empCheck = Utility.getRandomDouble();
+    public void attendance() {
+        double empCheck = Utility.getRandomInt();
         System.out.println(empCheck);
-
-        if(empCheck ==IS_FULL_TIME)
-        {
-            System.out.println("Employee is Present");
-            dailyEmpWage = WAGE_PER_HRS * fullDayHr;
-
+        int n =Utility.getRandomInt();
+        System.out.println(n);
+        int FullDayHr=8;
+        int PartTimeHr=4;
+        int WagePerHr=20;
+        switch(n) {
+            case 1:
+                System.out.println("present FullDayHr Emp");
+                System.out.println(WagePerHr * FullDayHr);
+                break;
+            case 2:
+                System.out.println("present PartTime Emp");
+                System.out.println(WagePerHr * PartTimeHr);
+                break;
+            default:
+                System.out.println("absent. ");
         }
-        else if(empCheck == IS_PART_TIME)
-        {
-            System.out.println("Employee is Present. ");
-            dailyEmpWage = WAGE_PER_HRS * partTimeHr;
-
-        }else {
-            System.out.println("Employee is Abscent. ");
-
-        }
-
-        System.out.println("Daily Wage of Employee::"+dailyEmpWage);
-
     }
-
-
 }
--- Utility.java
+++ Utility.java
@@ -1,7 +1,9 @@
 package com.program;
 public class Utility {
 
-    public static double getRandomDouble() {
-        return Math.floor(Math.random() * 10) % 3;
+
+
+    public static int getRandomInt() {
+        return (int)(Math.random() * 10) % 3;
     }
 }
