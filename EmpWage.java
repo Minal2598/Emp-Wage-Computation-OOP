76e67b748b63ec7a9bf9733a9b551d324e22988e
Minal <patilminalp@gmail.com>
2021-06-21 23:13:18 +0530

    Added:In this program we are Checking Employee Absent or Present.

--- EmpWage.java
+++ EmpWage.java
@@ -1,9 +1,13 @@
-
 package com.program;
 
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation....!!!");
+
+            System.out.println("Welcome to Employee Wage Computation");
+            Attendence attend = new Attendence(); //create object of class Attendance
+            attend.attendance();
+        }
     }
 
-}
+
+
