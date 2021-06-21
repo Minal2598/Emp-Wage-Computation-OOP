80ddc479b0f5f134adda5aaad1509d3c8db292a1
Minal <patilminalp@gmail.com>
2021-06-22 02:12:17 +0530

    Added:Calculating Wages for month

--- Attendence.java
+++ Attendence.java
@@ -6,22 +6,39 @@ public class Attendence extends EmpWage {
     public void attendance() {
         double empCheck = Utility.getRandomInt();
         System.out.println(empCheck);
-        int n =Utility.getRandomInt();
-        System.out.println(n);
+
         int FullDayHr=8;
         int PartTimeHr=4;
         int WagePerHr=20;
-        switch(n) {
-            case 1:
-                System.out.println("present FullDayHr Emp");
-                System.out.println(WagePerHr * FullDayHr);
-                break;
-            case 2:
-                System.out.println("present PartTime Emp");
-                System.out.println(WagePerHr * PartTimeHr);
-                break;
-            default:
-                System.out.println("absent. ");
+        int day=0;
+        int empHr=0;
+        int totalWage = 0;
+        //int n;
+        while (day <= 19) {
+
+            int n =Utility.getRandomInt();
+            switch (n) {
+                case 1:
+                    System.out.println("present fullday");
+                   totalWage = (totalWage + (WagePerHr * FullDayHr));
+
+                    day++;
+                    break;
+                case 2:
+                    System.out.println("present PartTime");
+                    totalWage = (totalWage + (WagePerHr * PartTimeHr));
+
+
+                    day++;
+                    break;
+                default:
+                    System.out.println("Employee is absent. ");
+                    totalWage = (totalWage + (WagePerHr * empHr));
+                    day++;
+
+            }
         }
+        System.out.println("total wage of a month is::" + totalWage);
+
     }
 }
